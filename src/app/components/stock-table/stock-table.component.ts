41b02665fa6e8c93c656797baf58e6a379676bb3
Kaue Cardinalli <kauecardinalli@hotmail.com>
2023-07-19 17:24:45 -0300

    refactor - CSS

--- src/app/components/stock-table/stock-table.component.ts
+++ src/app/components/stock-table/stock-table.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stock-table',
   templateUrl: './stock-table.component.html',
-  styleUrls: ['./stock-table.component.scss']
+  styleUrls: ['./stock-table.component.scss'],
 })
 export class StockTableComponent {
-
+  cerveja: string = 'Easy IPA';
+  estilo: string = 'IPA';
+  quantidade: number = 1;
+  local: string = 'Geladeira';
+  validade: string = '28/06/2023';
 }
