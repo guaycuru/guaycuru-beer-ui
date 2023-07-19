5664b9711cec688b50bc60dcda4946f9d36609e2
Felipe Guaycuru <guaycuru@gmail.com>
2023-07-19 11:30:56 -0300

    Adiciona componente feio motivador de Nandos e atualiza gitignore

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { ItemService } from '../core/services/item.service';
+import { Item } from '../core/models/item.model';
 
 @Component({
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  items: Item[];
 
-  constructor() { }
+  constructor(private itemService: ItemService) {
+  }
+
+  ngOnInit(): void {
+    void this.init();
+  }
+
+  private async init(): Promise<void> {
+    this.items = await firstValueFrom(this.itemService.listItems());
+  }
 
 }
