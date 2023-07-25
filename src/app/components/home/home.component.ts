91724bce33ca327230a3aa00f43ec26422211379
Felipe Guaycuru <guaycuru@gmail.com>
2023-07-25 11:43:17 -0300

    Remove codigo inutil do home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
-import { ItemService } from '../../core/services/item.service';
-import { Item } from '../../core/models/item.model';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
-  items: Item[];
-
-  constructor(private itemService: ItemService) {
-  }
-
-  ngOnInit(): void {
-    void this.init();
-  }
-
-  private async init(): Promise<void> {
-    this.items = await firstValueFrom(this.itemService.listItems());
-  }
-
+export class HomeComponent {
 }
